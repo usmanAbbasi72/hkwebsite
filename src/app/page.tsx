8dbf200c364c310b9a4c09a19ec3aca0839fff75
Piyar ali Abbasi <piyaraliabbasi52@gmail.com>
2026-01-17 14:05:18 +0000

    make home page full of fearuristic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { Contact } from '@/components/sections/Contact';
 import { Hero } from '@/components/sections/Hero';
+import { ProjectShowcase } from '@/components/sections/ProjectShowcase';
+import { Services } from '@/components/sections/Services';
 import { Stats } from '@/components/sections/Stats';
+import { Testimonials } from '@/components/sections/Testimonials';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Stats />
+      <Services />
+      <ProjectShowcase />
+      <Testimonials />
+      <Contact />
     </>
   );
 }
