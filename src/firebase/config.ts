9554dc59950db51090174f206b4130013eb496a6
Piyar ali Abbasi <piyaraliabbasi52@gmail.com>
2026-01-21 17:05:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-493228461-5868d",
+  "appId": "1:543383809554:web:2a17e1683806e2a4c832cb",
+  "apiKey": "AIzaSyAX2zDRYecsN5XIienioY6DVGP17i5fnwc",
+  "authDomain": "studio-493228461-5868d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "543383809554"
+};
